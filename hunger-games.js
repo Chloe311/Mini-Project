1b5facb278f7492bbac73fb628b647f6e1cc757c
Chloe311 <55338087+Chloe311@users.noreply.github.com>
2019-10-28 00:13:04 -0700

    Update hunger-games.js

--- hunger-games.js
+++ hunger-games.js
@@ -26,16 +26,16 @@ let scores = {
 
 //time record
 let timeLimits = {
-  timer: 20
+  timer: 15
 }
 
+//scene 0 sound
 let scene0Sound;
 
+//time countdown
 function getTimer() {
   if (frameCount % 60 !== 0) return timeLimits.timer;
-
   if (timeLimits.timer > 0) return timeLimits.timer--;
-
   return 0;
 }
 
@@ -48,74 +48,33 @@ function addScore(keyCode) {
   //console.log(types[keyCode]);
   let type = types[keyCode];
 
-  if (sceneIndex !== 1) return;//not in game
-  if (!type) return;//not correct key
+  if (sceneIndex !== 1) return; //not in game
+  if (!type) return; //not correct key
 
-  scores[type]++; 
+  scores[type]++;
   imgIndex[type] = 0;
-
-  // //Left Arrow
-  // if (timeLimits.timer >= 0 && keyCode === 37) {
-  //   scores.left++;
-  // } //Right Arrow
-  // else if (timeLimits.timer >= 0 && keyCode === 39) {
-  //   scores.right++;
-  // }
 }
 
 
 function getImg(type = 'left') {
-  if(imgIndex[type] >= imgs[type].length) return imgs[type][0];
-  
-  return imgs[type][imgIndex[type]++];//display next image
-  
-  // imgIndex.left++;
-  // return imgs.left[imgIndex.left];
+  if (imgIndex[type] >= imgs[type].length) return imgs[type][0];
+
+  return imgs[type][imgIndex[type]++]; //display next image
 }
 
-// function getImg(type = 39) {
-//   imgIndex.right++;
-//   return imgs.right[imgIndex.right];
-// }
-
-
 function drawScene0() {
-
-  image(coverImg,0,0);
-  //background(255);
+  image(coverImg, 0, 0);
   textSize(20);
-  text("Press Space to Start", width / 2, 380); 
-  
-  
+  text("Press Space to Start", width / 2, 380);
 }
 
 function drawScene1() {
-  scene1Sound.stop();
+  scene0Sound.stop();
   let time = getTimer();
   background(255);
 
-  //   //left image
-  //   image(imgs.left[0], 0, 200);
-  //   if (isTyped.left === 1 && imgIndex.left < imgs.left.length) {
-  //     image(imgs.left[imgIndex.left], 0, 200);
-  //     imgIndex.left++;
-  //     if (imgIndex.left == imgs.left.length) {
-  //       imgIndex.left = 0;
-  //     }
-  //     isTyped.left = 0;
-  //   }
-  image(getImg('left'), 0, 0, 300,400);
-
-  // //right image
-  //   image(imgs.right[imgIndex.right], 300, 200);
-  //   imgIndex.right++;
-  //   if (imgIndex.right == imgs.right.length - 1) {
-  //     imgIndex.right = 0;
-  //   }
-  //   isTyped.right = 0;
-  // }
-
-  image(getImg('right'), 300, 0, 300,400);
+  image(getImg('left'), 0, 0, 300, 400);
+  image(getImg('right'), 300, 0, 300, 400);
 
   textSize(15);
   text("Scores: " + scores.left, 120, 50);
@@ -135,43 +94,36 @@ function drawScene2() {
   background(255);
   textSize(50);
 
-  //let textStr = "";
-  
   if (scores.left > scores.right) {
-    image(endImgLeft, 0,0);
+    image(endImgLeft, 0, 0);
     textSize(30);
     text('Player 1 won !', width / 2, 60);
     textSize(15);
     text("Player 1 Scores: " + scores.left, 100, 250);
     text("Player 2 Scores: " + scores.right, 500, 250);
-    //textStr = "Player 1 won!";
-    //gameOver = endImg.left[0];
+
   } else if (scores.left < scores.right) {
-    image(endImgRight, 0,0);
+    image(endImgRight, 0, 0);
     textSize(30);
-    text('Player 2 won !', width / 2, 270);
+    text('Player 2 won !', width / 2, 60);
     textSize(15);
     text("Player 1 Scores: " + scores.left, 100, 250);
     text("Player 2 Scores: " + scores.right, 500, 250);
-    //textStr = "Player 2 won!";
-    //gameOver = endImg.right[0];
+
   } else {
-    image(endImgDraw, 0,0);
+    image(endImgDraw, 0, 0);
     textSize(30);
     text('DRAW', width / 2, 285);
     textSize(15);
     text("Player 1 Scores: " + scores.left, 100, 50);
     text("Player 2 Scores: " + scores.right, 500, 50);
-    //textStr = "Draw";
-    //gameOver = endImg.draw[0];
+
   }
 }
 
-
-
 function preload() {
   coverImg = loadImage('cover.png');
-  
+
   imgs.left.push(
     loadImage('pic5.png'),
     loadImage('pic6.png'),
@@ -186,14 +138,13 @@ function preload() {
     loadImage('pic4.png')
   );
 
-  endImgLeft =  loadImage('end.png');
+  endImgLeft = loadImage('end.png');
   endImgRight = loadImage('end2.png');
   endImgDraw = loadImage('draw.png');
 
   imgIndex.left = imgs.left.length;
   imgIndex.right = imgs.right.length;
-  
-  
+
   //sound
   soundFormats("mp3");
   scene0Sound = loadSound("scene0BG.mp3");
